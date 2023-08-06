097c7da0facf38102f949a249dab7ffc06d3af1a
oop778 <oskardhavel@gmail.com>
2023-08-06 11:57:41 +0300

    Remove unused classes

--- api/src/main/java/dev/oop778/shelftor/api/expiring/Expirable.java
+++
@@ -1,9 +0,0 @@
-package dev.oop778.shelftor.api.expiring;
-
-import java.util.function.Consumer;
-
-public interface Expirable<T> {
-    void onExpire(Consumer<T> expiryConsumer);
-
-    void invalidate();
-}
--- api/src/main/java/dev/oop778/shelftor/api/expiring/check/ExpirationCheck.java
+++
@@ -1,8 +0,0 @@
-package dev.oop778.shelftor.api.expiring.check;
-
-import dev.oop778.shelftor.api.shelf.expiring.ExpiringShelf;
-
-public interface ExpirationCheck<T> {
-
-    void onInitialization(ExpiringShelf<T> store);
-}
